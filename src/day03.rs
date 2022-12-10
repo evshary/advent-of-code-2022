a3ef5acdcf9c74435ac0f6aa16dab774b7c3a1a5
ChenYing Kuo <evshary@gmail.com>
2022-12-10 12:32:23 +0800

    Day03 part1 complete.

--- src/day03.rs
+++ src/day03.rs
@@ -1,15 +1,29 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::collections::HashMap;
 
 fn get_priority(a: &str, b: &str) -> u32 {
-    1
+    let mut idx = 0;
+    let mut v = [false; 52];
+    for c in a.chars() {
+        idx = c as usize;
+        idx -= if idx >= 97 { 97 } else { 65 - 26 };
+        v[idx] = true 
+    }
+    for c in b.chars() {
+        idx = c as usize;
+        idx -= if idx >= 97 { 97 } else { 65 - 26 };
+        if v[idx] == true {
+            break;
+        }
+    }
+    //println!("{}", idx + 1);
+    (idx + 1) as u32
 }
 
 fn main() {
     let mut priority = 0;
-    if let Ok(lines) = read_lines("input/day03_test.txt") {
+    if let Ok(lines) = read_lines("input/day03.txt") {
         for line in lines {
             if let Ok(content) = line {
                 let len = content.len();
